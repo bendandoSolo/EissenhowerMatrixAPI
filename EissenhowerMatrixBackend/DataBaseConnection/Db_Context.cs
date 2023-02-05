382d5355c967f75e52bd6ab0312ed67fd4771201
Ben Roberts <bendando@hotmail.co.uk>
2023-02-05 11:54:56 +0000

    initial commit: backend with in memory database

---
+++ EissenhowerMatrixBackend/DataBaseConnection/Db_Context.cs
@@ -0,0 +1,14 @@
+﻿using EissenhowerMatrixBackend.DataBaseConnection.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EissenhowerMatrixBackend.DataBaseConnection
+{
+        class TodoDb : DbContext
+        {
+            public TodoDb(DbContextOptions<TodoDb> options)
+                : base(options) { }
+
+            public DbSet<Todo> Todos => Set<Todo>();
+        }
+
+}
---
+++ EissenhowerMatrixBackend/Models/Todo.cs
@@ -0,0 +1,11 @@
+﻿namespace EissenhowerMatrixBackend.DataBaseConnection.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public string? Description { get; set; }
+        public DateTime? CompletionDate { get; set; }
+
+    }
+}
