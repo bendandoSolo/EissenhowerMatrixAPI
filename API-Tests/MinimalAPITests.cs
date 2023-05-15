81b2e08cde8b081d659ac172422a20e7baef5269
Ben Roberts <bendando@hotmail.co.uk>
2023-05-15 18:14:19 +0100

    adding test project and gitignore

---
+++ API-Tests/MinimalAPITests.cs
@@ -0,0 +1,83 @@
+﻿using EissenhowerMatrixBackend.DataBaseConnection.Models;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System.Net.Http.Json;
+using EissenhowerMatrixBackend.DataBaseConnection;
+
+public class TodoControllerTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+    private static TodoDb TodoDb { get; set; }
+
+    public TodoControllerTests(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddDbContext<TodoDb>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryTestDatabase");
+                });
+            });
+        });
+    }
+
+    [Fact]
+    public async Task TestGetTodoItems()
+    {
+        var client = _factory.CreateClient();
+        await SeedData(_factory.TodoDb);
+        var response = await client.GetAsync("/todoitems");
+
+        response.EnsureSuccessStatusCode();
+
+        var todoItems = await response.Content.ReadFromJsonAsync<Todo[]>();
+
+       //Assert.NotNull(todoItems);
+        //Assert.Equal(2, todoItems?.Length);
+    }
+
+    [Fact]
+    public async Task TestGetTodoItemsComplete()
+    {
+        // ... Your test logic here
+    }
+
+    [Fact]
+    public async Task TestGetTodoItemById()
+    {
+        // ... Your test logic here
+    }
+
+    [Fact]
+    public async Task TestPostTodoItem()
+    {
+        // ... Your test logic here
+    }
+
+    [Fact]
+    public async Task TestPutTodoItem()
+    {
+        // ... Your test logic here
+    }
+
+    [Fact]
+    public async Task TestDeleteTodoItem()
+    {
+        // ... Your test logic here
+    }
+
+    private async Task SeedData(TodoDb db)
+    {
+        // Seed your test data here
+        db.Todos.AddRange(
+            new Todo { Id = 1, Name = "Task 1", Description = "Description 1" },
+            new Todo { Id = 2, Name = "Task 2", Description = "Description 2", CompletionDate = DateTime.Now }
+        );
+
+        await db.SaveChangesAsync();
+    }
+}
+
---
+++ API-Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace API_Tests;
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
