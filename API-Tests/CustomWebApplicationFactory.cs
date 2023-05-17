926f1ec0546efb4b1c77f8d4fa22bdfca1a329d4
Ben Roberts <bendando@hotmail.co.uk>
2023-05-17 12:42:13 +0100

    WIP creating custom webapplication factory to share test inMemoryDatabase between all tests

---
+++ API-Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,66 @@
+﻿using EissenhowerMatrixBackend.DataBaseConnection.Models;
+using EissenhowerMatrixBackend.DataBaseConnection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_Tests;
+
+public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+{
+    public CustomWebApplicationFactory()
+    {
+        this.ClientOptions.BaseAddress = new Uri("http://localhost/api");
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            // Add a database context (AppDbContext) using an in-memory database for testing.
+            services.AddDbContext<TodoDb>(options =>
+            {
+                options.UseInMemoryDatabase("InMemoryTestDatabase");
+            });
+
+            // Build the service provider.
+            var serviceProvider = services.BuildServiceProvider();
+
+            // Create a scope to obtain a reference to the database context (AppDbContext).
+            using (var scope = serviceProvider.CreateScope())
+            {
+                var scopedServices = scope.ServiceProvider;
+                var db = scopedServices.GetRequiredService<TodoDb>();
+
+                // Ensure the database is created.
+                db.Database.EnsureCreated();
+
+                try
+                {
+                    // Seed the database with test data.
+                    SeedData(db);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Exception occurred while seeding the database with test data. Exception: {ex.Message}");
+                }
+            }
+        });
+    }
+
+    private void SeedData(TodoDb db)
+    {
+        db.Todos.AddRange(
+            new Todo { Id = 1, Name = "Task 1", Description = "Description 1" },
+            new Todo { Id = 2, Name = "Task 2", Description = "Description 2", CompletionDate = DateTime.Now }
+        );
+
+        db.SaveChanges();
+    }
+}
--- API-Tests/MinimalAPITests.cs
+++ API-Tests/MinimalAPITests.cs
@@ -1,10 +1,11 @@
-﻿using EissenhowerMatrixBackend.DataBaseConnection.Models;
+﻿
+using EissenhowerMatrixBackend.DataBaseConnection.Models;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.TestPlatform.TestHost;
 using System.Net.Http.Json;
 using EissenhowerMatrixBackend.DataBaseConnection;
-
+namespace API_Tests;
 public class TodoControllerTests : IClassFixture<WebApplicationFactory<Program>>
 {
     private readonly WebApplicationFactory<Program> _factory;
@@ -26,13 +27,14 @@ public class TodoControllerTests : IClassFixture<WebApplicationFactory<Program>>
         // Get a service scope to get the TodoDb instance
         var scope = _factory.Services.CreateScope();
         TodoDb = scope.ServiceProvider.GetRequiredService<TodoDb>();
+
     }
 
     [Fact]
     public async Task TestGetTodoItems()
     {
         var client = _factory.CreateClient();
-        await SeedData(TodoDb);
+        //await SeedData(TodoDb);
         var response = await client.GetAsync("/todoitems");
 
         response.EnsureSuccessStatusCode();
@@ -42,37 +44,93 @@ public class TodoControllerTests : IClassFixture<WebApplicationFactory<Program>>
        Assert.NotNull(todoItems);
        Assert.Equal(2, todoItems?.Length);
     }
-
     [Fact]
-    public async Task TestGetTodoItemsComplete()
+    public async Task GetAPI_CanGetAllTodos()
     {
-        // ... Your test logic here
+        // Arrange
+        var client = _factory.CreateClient();
+         await SeedData(TodoDb);
+
+        // Act
+        var response = await client.GetAsync("/todoitems/complete");
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        var todoItems = await response.Content.ReadFromJsonAsync<Todo[]>();
+        Assert.Contains(todoItems, item => item.CompletionDate != null);
+        // Assert.All(todoItems, item => Assert.True(item.CompletionDate != null));
     }
 
     [Fact]
-    public async Task TestGetTodoItemById()
+    public async Task GetAPI_CanGetItemById()
     {
-        // ... Your test logic here
+        // Arrange
+        var client = _factory.CreateClient();
+       // await SeedData(TodoDb);
+        var id = 1; // assuming you have an item with Id 1
+
+        // Act
+        var response = await client.GetAsync($"/todoitems/{id}");
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        var todoItem = await response.Content.ReadFromJsonAsync<Todo>();
+        Assert.Equal(id, todoItem?.Id);
     }
 
     [Fact]
     public async Task TestPostTodoItem()
     {
-        // ... Your test logic here
+        // Arrange
+        var client = _factory.CreateClient();
+        var newTodo = new Todo { Name = "New Task", Description = "New Description" };
+
+        // Act
+        var response = await client.PostAsJsonAsync("/todoitems", newTodo);
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        var todoItem = await response.Content.ReadFromJsonAsync<Todo>();
+        Assert.Equal(newTodo.Name, todoItem?.Name);
+        Assert.Equal(newTodo.Description, todoItem?.Description);
     }
 
     [Fact]
     public async Task TestPutTodoItem()
     {
-        // ... Your test logic here
+        // Arrange
+        var client = _factory.CreateClient();
+       // await SeedData(TodoDb);
+        var existingTodo = new Todo { Id = 3, Name = "Updated Task", Description = "Updated Description" }; // assuming an item with Id 1 exists
+
+        // Act
+        var response = await client.PutAsJsonAsync($"/todoitems/{existingTodo.Id}", existingTodo);
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        var todoItem = await response.Content.ReadFromJsonAsync<Todo>();
+        Assert.Equal(existingTodo.Name, todoItem?.Name);
+        Assert.Equal(existingTodo.Description, todoItem?.Description);
     }
 
     [Fact]
     public async Task TestDeleteTodoItem()
     {
-        // ... Your test logic here
-    }
+        // Arrange
+        var client = _factory.CreateClient();
+        // await SeedData(TodoDb);
+        var id = 1; // assuming you have an item with Id 1
 
+        // Act
+        var response = await client.DeleteAsync($"/todoitems/{id}");
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+
+        // Verify the item is deleted
+        var getResponse = await client.GetAsync($"/todoitems/{id}");
+        Assert.False(getResponse.IsSuccessStatusCode);
+    }
     private async Task SeedData(TodoDb db)
     {
         // Seed your test data here
