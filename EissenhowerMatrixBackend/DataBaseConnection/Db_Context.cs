bacd8d3f47bc0489805382c8ffefa89e6ae44a0c
Ben Roberts <bendando@hotmail.co.uk>
2023-05-19 15:27:45 +0100

    Add: EissenhowerStatus to TodoItem

--- EissenhowerMatrixBackend/DataBaseConnection/Db_Context.cs
+++ EissenhowerMatrixBackend/DataBaseConnection/Db_Context.cs
@@ -1,11 +1,19 @@
 ﻿namespace EissenhowerMatrixBackend.DataBaseConnection;
 using EissenhowerMatrixBackend.DataBaseConnection.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 public class TodoDb : DbContext
 {
     public TodoDb(DbContextOptions<TodoDb> options)
         : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Todo>()
+            .Property(e => e.Priority)
+            .HasConversion(new EnumToStringConverter<EissenhowerStatus>());
+    }
 
     public DbSet<Todo> Todos => Set<Todo>();
 }
--- EissenhowerMatrixBackend/Models/Todo.cs
+++ EissenhowerMatrixBackend/Models/Todo.cs
@@ -6,5 +6,15 @@ public class Todo
     public string Name { get; set; } = "";
     public string? Description { get; set; }
     public DateTime? CompletionDate { get; set; }
+    public EissenhowerStatus Priority { get; set; } = EissenhowerStatus.Unassigned;
+}
 
+// can use description tags on enums and custom GetDescription method to get the description to include spaces in description
+public enum EissenhowerStatus
+{
+    Unassigned,
+    UrgentPriority,
+    NotUrgentPriority,
+    UrgentLowPriority,
+    NotUrgentLowPriority,
 }
